53900c8fa0d0629e5bb73e9a5f1b128adb05085f
takhyun12 <takhyun12@gmail.com>
2021-06-18 11:16:30 +0900

    Gui code refactoring

--- usage_gui.py
+++ usage_gui.py
@@ -1,50 +1,59 @@
 import datetime
-import tkinter as tk  # GUI
-from tkinter import ttk  # GUI
-from tkinter import messagebox # GUI
-
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
 from repeated_timer import Repeated_Timer
 
-class AppGui(tk.Tk):
+
+class App(tk.Tk):
     def __init__(self):
-        """ GUI Setting Starts Here """
-        super().__init__()  # Inheritance from tkinter class
-        self.title("Timer Sample")
-        self.geometry("600x100")
+        super().__init__()
+
+        self.title("Repeated timer with Tkinter")
+        self.geometry("600x250")
         self.resizable(0, 0)
 
-        self.start_button = tk.Button(self, overrelief="solid", width=15, text="Start", repeatdelay=1000, repeatinterval=100,
-                                 command=lambda: self.start_button_clicked())
-        self.start_button.grid(column=0, row=3)
-        """ GUI Setting Ends Here """
+        self.repeated_timer = None  # Declares a repeated_timer variable inside a Tk object.
+
+        self.start_button = tk.Button(self, text="Start", width=30, overrelief="solid",
+                                      font=('Roboto', 16, 'bold'),
+                                      command=lambda: self.start_button_clicked())
+        self.start_button.pack()
 
     def start_button_clicked(self):
-        # If Tick Starts, start a new thread
         if self.start_button['text'] == 'Start':
-            # Set Timer
-            self.repeated_timer = Repeated_Timer(interval=1, duration=30, function=self.timer_tick_event, args1='args1', args2='args2')
-            self.repeated_timer.start()
+            '''Start Event'''
+            self.repeated_timer = Repeated_Timer(interval=1, duration=30, function=timer_tick,
+                                                 args1='args1', args2='args2')  # Assign object to variable repeated_timer
+            self.repeated_timer.start()   # Timer Start
 
             self.start_button['text'] = 'Stop'
             self.start_button.configure(fg='red')
 
-        # If Tick Stopped, initialize everything
-        # + Once variables are initialized, pre-exist thread will be stopped automatically
         else:
-            self.repeated_timer.stop()
+            '''Stop Event'''
+            self.repeated_timer.stop()  # Timer Stop
 
             self.start_button['text'] = 'Start'
             self.start_button.configure(fg='black')
 
-    def timer_tick_event(self, *args: tuple, **kwargs: tuple):
-        print('timer tick!\t', datetime.datetime.now())
-
-    def on_closing(self):
+    def closing_event(self):
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            self.repeated_timer.stop()
-            self.destroy()
+            try:
+                self.repeated_timer.stop()
+            except AttributeError:
+                pass
+            finally:
+                self.destroy()
+
+
+def timer_tick(*args: tuple, **kwargs: tuple):
+    # timer tick event!!
+    # You can put your code in here
+    print('timer tick!')
+
 
 if __name__ == '__main__':
-    app = AppGui()
-    app.protocol("WM_DELETE_WINDOW", app.on_closing)
-    app.mainloop()  # execute GUI
+    app = App()
+    app.protocol("WM_DELETE_WINDOW", app.closing_event)
+    app.mainloop()
